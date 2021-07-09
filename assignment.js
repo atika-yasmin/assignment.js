885a0510caef0b314a9792c88859be9d5903fd9d
atika-yasmin <atika.yasmin.19@gmail.com>
2021-07-08 22:16:37 -0700

    three functions are finished

---
+++ assignment.js
@@ -0,0 +1,54 @@
+//https://github.com/atika-yasmin/assignment.js
+
+//Question-01
+function kilometerToMeter(kilometer){
+    var meter = kilometer*1000;
+    if(kilometer < 0){
+        return "This is not valid.";
+    }
+    else{
+        return meter;
+    }
+}
+console.log(kilometerToMeter(10));
+
+//Question-02
+function budgetCalculator(watch, phone, laptop){
+    var totalbudget = (watch * 50) + (phone * 100) + (laptop * 500);
+    if (watch < 0 || phone < 0 || laptop < 0)
+    {
+        return "Please enter right amount."
+    }
+    else
+    {
+        return totalbudget;
+    }
+}
+console.log(budgetCalculator(3,5,2));
+
+//Question-03
+function hotelCost(days){
+    var totalCost;
+    if(days < 0 || days == null){
+        return "Days cannot be negative.";
+    }
+    else{
+        if(days <= 10){
+            totalCost = days * 100;
+        }
+        else if(days >10 && days <= 20){
+            first10days = 10 * 100;
+            remainingDays = days - 10;
+            totalCost = first10days + (remainingDays*80);
+        }
+        else{
+            first10days = 10 * 100;
+            second10days = 10 * 80;
+            remainingDays = days - 20;
+            totalCost = first10days + second10days + (remainingDays*50);
+        }
+    }
+    return totalCost;
+}
+console.log(hotelCost(23));
+
