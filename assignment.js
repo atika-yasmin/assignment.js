d962012e6b68820c1c64e9fe4dad0a2da6139654
atika-yasmin <atika.yasmin.19@gmail.com>
2021-07-09 08:40:51 -0700

    four functions have done

--- assignment.js
+++ assignment.js
@@ -10,7 +10,7 @@ function kilometerToMeter(kilometer){
         return meter;
     }
 }
-console.log(kilometerToMeter(10));
+//console.log(kilometerToMeter(10));
 
 //Question-02
 function budgetCalculator(watch, phone, laptop){
@@ -24,11 +24,12 @@ function budgetCalculator(watch, phone, laptop){
         return totalbudget;
     }
 }
-console.log(budgetCalculator(3,5,2));
+//console.log(budgetCalculator(3,5,2));
 
 //Question-03
 function hotelCost(days){
-    var totalCost;
+    var first10days, second10days, remainingDays;
+    var totalCost=0;
     if(days < 0 || days == null){
         return "Days cannot be negative.";
     }
@@ -50,5 +51,29 @@ function hotelCost(days){
     }
     return totalCost;
 }
-console.log(hotelCost(23));
+//console.log(hotelCost(23));
+
+//Question-04
+function megaFriend(friendsName) {
+    var newFriendsName,i;
+    var largestName = friendsName[0];
+    if(friendsName.length === 0)
+    {
+        return "Please inter a valid array.";
+    }
+    else{
+        for(i=0; i<friendsName.length; i++){
+            newFriendsName = friendsName[i];
+            if(typeof newFriendsName != "string"){
+                return "Only string is accepted."
+            }
+            else if(largestName.length < newFriendsName.length){
+                largestName = newFriendsName;
+            }
+        }
+    }
+    return largestName;
+}
+//var NameList = ["rozerin", "atika", "arif", "mobasshira", "khusbu", "roshni"];
+//console.log(megaFriend(NameList));
 
